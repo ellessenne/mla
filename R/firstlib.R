f13745634eab82e071f19be69b6c36f02ac2df67
Alessandro Gasparini <alessandro.gasparini@ki.se>
2019-09-24 17:18:04 +0200

    Removed some unnecessary stuff

--- R/firstlib.R
+++
@@ -1,8 +0,0 @@
-# .onLoad <- function(lib, pkg) {
-#   library.dynam("mla", pkg, lib)
-# }
-#
-# .onUnload <- function(libpath)
-#   library.dynam.unload("mla", libpath)
-#
-# ############ End of .First.lib ###############
