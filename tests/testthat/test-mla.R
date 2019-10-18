50d27ae638dfd5513723b4e28ef82d5d0c81eba2
Alessandro Gasparini <alessandro.gasparini@ki.se>
2019-10-18 16:33:31 +0200

    Should not support passing the hessian, only gradienthave the hessian

--- tests/testthat/test-mla.R
+++ tests/testthat/test-mla.R
@@ -8,9 +8,6 @@ f1 <- function(b) {
 gr <- function(b) {
   return(c(8 * (b[1] - 5), 2 * (b[2] - 6)))
 }
-hes <- function(b) {
-  return(c(-8, 0, -2))
-}
 
 testthat::test_that("Test #1.1", {
   t1.1 <- mla::mla(par = c(8, 9), control = ctrl, fn = f1)
@@ -21,17 +18,6 @@ testthat::test_that("Test #1.2 (with gradient)", {
   t1.2 <- mla::mla(par = c(8, 9), control = ctrl, fn = f1, gr = gr)
   testthat::expect_equal(object = t1.2$par, expected = c(5, 6), tolerance = 1e-6)
 })
-
-testthat::test_that("Test #1.3 (with hessian)", {
-  t1.3 <- mla::mla(par = c(8, 9), control = ctrl, fn = f1, hessian = hes)
-  testthat::expect_equal(object = t1.3$par, expected = c(5, 6), tolerance = 1e-6)
-})
-
-testthat::test_that("Test #1.4 (with gradient and hessian)", {
-  t1.4 <- mla::mla(par = c(8, 9), control = ctrl, fn = f1, gr = gr, hessian = hes)
-  testthat::expect_equal(object = t1.4$par, expected = c(5, 6), tolerance = 1e-5)
-})
-
 
 ### Second test
 f2 <- function(b) {
